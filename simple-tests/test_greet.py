1e19eb235b7fcaab427c5cf65a05068009e6055d
ctarrington <arrington.ct@gmail.com>
2018-08-03 22:09:16 -0400

    python 3 types and eda

--- simple-tests/test_greet.py
+++ simple-tests/test_greet.py
@@ -3,3 +3,7 @@ from greet import greet
 
 def test_greet():
     assert greet('Fred') == 'Hi there, Fred'
+
+
+def test_fancy():
+    assert greet('Frederick the Great') == 'Hi there, Frederick the Great'
