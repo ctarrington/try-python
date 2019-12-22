a5eb4ce2872e4db5733cd205504c2b045f3848e6
CT Arrington <arrington.ct@gmail.com>
2019-12-21 20:01:00 -0500

    filtering

--- try-pandas/basics.py
+++ try-pandas/basics.py
@@ -4,5 +4,11 @@ print(pd.__version__)
 
 df = pd.read_csv('./data/gapminder.tsv', sep='\t')
 print(df.head())
+print(df.info())
 
+slice = df[['country', 'year', 'pop']]
+print(slice.head())
+
+just60s = df[(df.year >= 1960) & (df.year <= 1969)]
+print(just60s.iloc[0:10, [0,2]])
 
